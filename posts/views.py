5934f9fc31cde6241487bdf08859cb3f7b92d5e6
ikinoti <gitongakinoti1@gmail.com>
2024-04-26 01:24:44 +0300

    modified our view from function based to class based views

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 
 from .models import Post
 from .serializers import PostSerializer
@@ -24,16 +24,28 @@ def homepage(request:Request):
     return Response(data=response, status=status.HTTP_200_OK)
 
 
-@api_view(http_method_names=["GET", "POST"])
-def list_posts(request:Request):
-    posts = Post.objects.all()
+class PostListCreateView(APIView):
 
-    if request.method == "POST":
+    
+    """
+        a view for creating and listing posts
+    """
+    
+    serializer_class = PostSerializer
+
+    def get(self, request:Request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = self.serializer_class(instance=posts, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request:Request, *args, **kwargs):
         data = request.data
-        serializer = PostSerializer(data=data)
+        serializer = self.serializer_class(data=data)
 
         if serializer.is_valid():
             serializer.save()
+
             response = {
                 "message": "Post Created",
                 "data": serializer.data
@@ -41,52 +53,37 @@ def list_posts(request:Request):
 
             return Response(data=response, status=status.HTTP_201_CREATED)
         
-
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    serializer = PostSerializer(instance=posts, many=True)
-
-    response = {
-        "message":"post",
-        "data": serializer.data
-    }
-
-    return Response(data=response, status=status.HTTP_200_OK)
-
-@api_view(http_method_names=["GET"])
-def post_detail(request:Request, post_id:int):
-    post = get_object_or_404(Post, pk=post_id)
-    serializer = PostSerializer(instance=post)
-
-    response = {
-        "message":"post",
-        "data": serializer.data
-    }
-
-    return Response(data=response, status=status.HTTP_200_OK)
 
 
+class PostRetrieveUpdateDeleteView(APIView):
+    serializer_class = PostSerializer
 
-@api_view(http_method_names=["PUT"])
-def update_post(request:Request, post_id:int):
-    post = get_object_or_404(Post, pk=post_id)
-    data = request.data
-    serializer = PostSerializer(instance=post, data=data)
+    def get(self, request:Request, post_id:int):
+        post = get_object_or_404(Post, pk=post_id)
+        serializer = self.serializer_class(instance=post)
 
-    if serializer.is_valid():
-        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-        response = {
-            "message":"Post Update Successfully",
-            "data": serializer.data
-        }
+    def put(self, request:Request, post_id:int):
+        post = get_object_or_404(Post, pk=post_id)
+        data = request.data
+        serializer = self.serializer_class(instance=post, data=data)
 
-        return Response(data=response, status=status.HTTP_200_OK)
-    
-    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
 
-@api_view(http_method_names=["DELETE"])
-def delete_post(request:Request, post_id:int):
-    post = get_object_or_404(Post, pk=post_id)
-    post.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+            response = {
+                "message": "Post Updated",
+                "data": serializer.data
+            }
+
+            return Response(data=response, status=status.HTTP_200_OK)
+        
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request:Request, post_id:int):
+        post = get_object_or_404(Post, pk=post_id)
+        post.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
