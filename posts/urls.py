30ba3a13f9579aae0e7f42d2486428a3e214396e
ikinoti <gitongakinoti1@gmail.com>
2024-04-26 02:32:02 +0300

    change path

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("homepage/", views.homepage, name="posts_home"),
     
     path("",views.PostListCreateView.as_view(), name="list_post"),
-    path("<int:post_id>", views.PostRetrieveUpdateDeleteView.as_view(), name="post_detail"),
+    path("<int:pk>", views.PostRetrieveUpdateDeleteView.as_view(), name="post_detail"),
     
 ]
