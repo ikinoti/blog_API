9614383dce5487461e69f32feb75e2afdf679774
ikinoti <gitongakinoti1@gmail.com>
2024-04-30 02:11:01 +0300

    implemented login class view

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from .serializers import SignUpSerializer
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
+
 
 # Create your views here.
 
 class SignUpView(generics.GenericAPIView):
     serializer_class = SignUpSerializer
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [SessionAuthentication, TokenAuthentication ]
 
     def post(self, request:Request):
         data = request.data
@@ -24,3 +31,32 @@ class SignUpView(generics.GenericAPIView):
             return Response(data=response, status=status.HTTP_201_CREATED)
         
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [SessionAuthentication, TokenAuthentication, ]
+    
+
+    def post(self, request:Request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(email=email, password = password)
+
+        if user is not None:
+            response = {
+                "message": "Login Successfull",
+                "token": user.auth_token.key
+            }
+            return Response(data=response, status=status.HTTP_200_OK)
+        else:
+            return Response(data={"Message": "Invalid email or password"})
+
+    def get(self, request:Request):
+        content = {
+            "user": str(request.user),
+            "auth": str(request.auth)
+        }
+
+        return Response(data=content, status=status.HTTP_200_OK)
