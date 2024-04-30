e941edf979cd009c23c1472a2c9589d3900ae7c4
ikinoti <gitongakinoti1@gmail.com>
2024-05-01 00:58:26 +0300

    view to c=get user and their posts

--- posts/views.py
+++ posts/views.py
@@ -4,12 +4,14 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, generics, mixins
-from rest_framework.decorators import api_view, APIView
+from rest_framework.decorators import api_view, APIView, permission_classes
 
 from .models import Post
 from .serializers import PostSerializer
+from accounts.serializers import CurrentUserPostsSerializer
 
 from django.shortcuts import get_object_or_404
+
 
 
 
@@ -34,6 +36,11 @@ class PostListCreateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.
     permission_classes = [IsAuthenticated,]
     queryset = Post.objects.all()
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(author=user)
+        return super().perform_create(serializer)
+
     def get(self, request:Request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
@@ -41,9 +48,16 @@ class PostListCreateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.
         return self.create(request, *args, **kwargs)
 
 
-class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class PostRetrieveUpdateDeleteView(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+    ):
+
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get(self, request:Request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -53,3 +67,18 @@ class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModel
 
     def delete(self, request:Request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+    
+@api_view(http_method_names=['GET'])
+@permission_classes([IsAuthenticated])
+def get_posts_for_current_user(request:Request):
+    user = request.user
+
+    serializer = CurrentUserPostsSerializer(
+        instance=user,
+        context = {"request":request}
+        )
+
+    return Response(
+        data= serializer.data,
+        status=status.HTTP_200_OK
+    )
