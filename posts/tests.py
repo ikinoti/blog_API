7b664cb24712d36d141f013bf740a56b5c237614
ikinoti <gitongakinoti1@gmail.com>
2024-05-02 01:55:15 +0300

    test using api client test case

--- posts/tests.py
+++ posts/tests.py
@@ -15,18 +15,44 @@ class HelloWorldTestCase(APITestCase):
 
 class PostListCreateTestCase(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = PostListCreateView.as_view()
+        # self.factory = APIRequestFactory()
+        # self.view = PostListCreateView.as_view()
         self.url = reverse("list_posts")
-        self.user = User.objects.create(
-            username = "janedoe",
-            email = "janedoe@app.com",
-            password = "password@1234"
+        # self.user = User.objects.create(
+        #     username = "janedoe",
+        #     email = "janedoe@app.com",
+        #     password = "password@1234"
+        # )
+
+    def authenticate(self):
+        self.client.post(
+            reverse('signup'),
+            {
+                "email": "janedoe@app.com",
+                "password": "password@1234",
+                "username": "janedoe",
+            },
         )
 
+        response = self.client.post(
+            reverse('login'),
+            {
+               "email": "janedoe@app.com",
+               "password": "password@1234", 
+            }
+            )
+        # print(response.data)
+
+        token = response.data['tokens']['access']
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {token}')
+
     def test_list_posts(self):
-        request = self.factory.get(self.url)
-        response = self.view(request)
+        # using apifactory
+        # request = self.factory.get(self.url)
+        # response = self.view(request)
+
+        # using apiclient
+        response = self.client.get(self.url)
 
         # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -38,9 +64,16 @@ class PostListCreateTestCase(APITestCase):
             "title": "Sample post",
             "content": "Sample content"
         }
-        request = self.factory.post(self.url, sample_post)
-        request.user = self.user
-        response = self.view(request)
+        # request = self.factory.post(self.url, sample_post)
+        # request.user = self.user
+        # response = self.view(request)
+
+        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # using api client
+        self.authenticate()
+        response = self.client.post(self.url, sample_post)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], sample_post['title'])
 
