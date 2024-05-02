1691ae9ddc035b21cafe80713b96f30090a4acac
ikinoti <gitongakinoti1@gmail.com>
2024-05-03 01:55:24 +0300

    swagger config on urls

--- blog_API/urls.py
+++ blog_API/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="This is an API for a Blogging Application",
+      
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path("posts/", include("posts.urls")),
     path("auth/", include("accounts.urls")),
